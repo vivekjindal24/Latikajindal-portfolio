263830c6f0aacfb21095bd773dbcc2947b998d3f
vivek <vivek.jindal.sbg@gmail.com>
2025-10-31 11:19:05 +0530

    Add Azure Static Web Apps deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Azure Static Web Apps supports Next.js SSG/SSR
+  // No need for static export
+}
 
 module.exports = nextConfig
