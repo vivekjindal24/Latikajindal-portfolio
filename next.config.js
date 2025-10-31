78da73b6f25674fb31671a5a5f4921a9de5044ad
vivek <vivek.jindal.sbg@gmail.com>
2025-10-31 12:37:57 +0530

    chore: remove Azure Static Web Apps workflows and config; generalize Next.js config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Azure Static Web Apps supports Next.js SSG/SSR
-  // No need for static export
-}
+const nextConfig = {}
 
 module.exports = nextConfig
